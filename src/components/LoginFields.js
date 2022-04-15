0d82110a272644c43a061730a6ea2e5d4e70bb44
Chris Gallo <chzvalrmd17@gmail.com>
2022-04-15 15:25:22 -0600

    Updated sign in ui

--- src/components/LoginFields.js
+++ src/components/LoginFields.js
@@ -1,53 +1,84 @@
-import { Button } from "@mui/material"
+import { Button, Box, Avatar, Typography, TextField, Grid, Link, CssBaseline } from "@mui/material"
+import { LockOutlined } from "@mui/icons-material";
 import { Form, Container } from "react-bootstrap"
 import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 export default function LoginFields ({ login, setEmail, setPassword, user }) {
   let navigate = useNavigate();
+  const theme = createTheme();
 
   const onSubmit = (event) => {
     event.preventDefault();
     login()
     navigate("../feed")
   };
-
+  
   return (
-    <Container className='mt-3'>
-      <Form onSubmit={onSubmit}> 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control 
-            type="email" 
-            placeholder="Enter email" 
-            onChange={(event) => { setEmail(event.target.value) }}
-          />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control 
-            type="password" 
-            placeholder="Password" 
-            onChange={(event) => { setPassword(event.target.value) }}
-          />
-        </Form.Group>
-        <Button variant="contained" type="submit">
-          Sign in
-        </Button>
-      </Form>
-
-      
-      {/* <TextField
-        id="liPassword"
-        type="password"
-        className="m-2"
-        onChange={(event) => { setPassword(event.target.value) }}
-        label="Password"
-      /> */}
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={onSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              onChange={(event) => { setEmail(event.target.value) }}
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(event) => { setPassword(event.target.value) }}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              {/* <Grid item xs justifyContent="flex-start">
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid> */}
+              <Grid item>
+                <Link href="../register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
       <h4>{user?.email}</h4>
-    </Container>
+    </ThemeProvider>
   )
 }
