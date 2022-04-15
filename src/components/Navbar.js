8a7661f9ddc41086531ab0b2a6a4a75569f8b5d7
Chris Gallo <chzvalrmd17@gmail.com>
2022-04-15 13:40:16 -0600

    Installed react router dependency

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,36 @@
 import { Nav, Navbar, Container } from "react-bootstrap";
 import logo from '../img/logo-white.png'
 import AuthButton from "./AuthButton";
+import { Link } from 'react-router';
 
 export default function NavBar ({login, logout, register, user}) {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src={logo}
-            height="35"
-            className="d-inline-block align-top"
-          />{' '}
+        <Navbar.Brand >
+          <Link to="/" className="Nav__brand">
+            <img
+              alt=""
+              src={logo}
+              height="35"
+              className="d-inline-block align-top"
+            />{' '}
+          </Link>
         </Navbar.Brand>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-          <Nav.Link href="#deets">More deets</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Dank memes
-          </Nav.Link>
+          <Nav.Link href="#feed">Feed</Nav.Link>
+          <Nav.Link eventKey={2} href="#login">Login</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-      <AuthButton login={()=>login()} logout={()=>logout()} register={()=>register()} user={user} />
+
+      <AuthButton 
+        login={()=>login()} 
+        logout={()=>logout()} 
+        register={()=>register()} 
+        user={user} 
+      />
+
       </Container>
     </Navbar>
   )
