ccdb59688829598823a8dd23dbf1e505352517f3
Chris Gallo <chzvalrmd17@gmail.com>
2022-04-25 22:03:15 -0600

    updated arrow buttons on quotev2 and added looping functinality

--- src/components/QuoteV2.js
+++ src/components/QuoteV2.js
@@ -1,19 +1,28 @@
-import { Card } from "react-bootstrap"
-import { MoreVert, KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
-import { IconButton, MobileStepper, Button } from "@mui/material";
+import { Card, Row, Col } from "react-bootstrap"
+import { MoreVert, ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import { IconButton, MobileStepper, Button, Stack } from "@mui/material";
 import { useState } from "react";
 import { useTheme } from "@mui/material/styles";
 
 export default function QuoteV2 () {
   const theme = useTheme
   const [activeStep, setActiveStep] = useState(0);
+  const numberOfQuotes = 6;
 
   const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    if (activeStep + 1 === numberOfQuotes) {
+      setActiveStep(0);
+    } else {
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    }
   };
-
+  
   const handleBack = () => {
+    if (activeStep === 0) {
+      setActiveStep(numberOfQuotes - 1);
+    } else {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    }
   };
 
   return (
@@ -24,34 +33,51 @@ export default function QuoteV2 () {
       </Card.Header>
 
       <Card.Body align='left' className="p-0">
-        <Card.Text className="p-3 m-0" align="center">
-          "This is where the quote would be"
-        </Card.Text>
+
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          className="p-3 m-0"
+          spacing={2}>
+          <IconButton onClick={handleBack}>
+            <ArrowBackIos color="primary"/>
+          </IconButton> 
+          <Card.Text align="center">
+            "This is where the quote would be"
+          </Card.Text>
+          <IconButton onClick={handleNext}>
+            <ArrowForwardIos color="primary"/>
+          </IconButton> 
+
+        </Stack>
+        
         <MobileStepper
-          className="p-1"
+          className="pb-2"
           variant="dots"
-          steps={6}
+          steps={numberOfQuotes}
           position="static"
           activeStep={activeStep}
           sx={{ flexGrow: 1 }}
-          nextButton={
-            <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
-              {theme.direction === 'rtl' ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-              {theme.direction === 'rtl' ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-            </Button>
-          }
+          style={{justifyContent: "center"}}
+          // nextButton={
+          //   <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+          //     {theme.direction === 'rtl' ? (
+          //       <KeyboardArrowLeft />
+          //     ) : (
+          //       <KeyboardArrowRight />
+          //     )}
+          //   </Button>
+          // }
+          // backButton={
+          //   <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          //     {theme.direction === 'rtl' ? (
+          //       <KeyboardArrowRight />
+          //     ) : (
+          //       <KeyboardArrowLeft />
+          //     )}
+          //   </Button>
+          // }
         />
       </Card.Body>
     </Card>
